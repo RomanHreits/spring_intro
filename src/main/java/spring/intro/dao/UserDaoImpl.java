364cca7aa3351b22e9fd9eb5b344019b8d78c695
romanHreits <romagreic@gmail.com>
2020-10-17 21:31:33 +0300

    solution hw-spring-intro

---
+++ src/main/java/spring/intro/dao/UserDaoImpl.java
@@ -0,0 +1,46 @@
+package spring.intro.dao;
+
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.stereotype.Repository;
+import spring.intro.model.User;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+    private final SessionFactory sessionFactory;
+
+    public UserDaoImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    public void add(User user) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(user);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't insert to DB user: " + user);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    public List<User> getUsers() {
+        try {
+            Session session = sessionFactory.openSession();
+            return session.createQuery("from User", User.class).getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get users from DB!", e);
+        }
+    }
+}
